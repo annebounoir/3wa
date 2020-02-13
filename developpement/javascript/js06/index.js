743b96f6a3d1531bf5e877efacf894df837169d6
anne <anne.bounoir@gmail.com>
2020-02-13 09:39:23 +0100

    ajout queryselector

--- developpement/javascript/js06/index.js
+++ developpement/javascript/js06/index.js
@@ -1,29 +1,24 @@
 // Receuillir les valeurs des input et du formulaire//
 
-
 let formDonnees = document.querySelector('form');
 let firstName = document.getElementById("first-name");
 let lastName = document.getElementById("last-name");
 let email= document.getElementById("email");
 formDonnees.addEventListener("submit", function(event) {
   event.preventDefault();
-  console.log(firstName + lastName + email);
+  console.log(firstName.value);
+  console.log(lastName.value);
+  console.log(email.value);
 });
 
+// lien //
 
-// lien // Empêcher le chargement de le page //
-
-function donnees(page){
-
-    page.preventDefault();
-    let firstName = document.querySelector("first-name");
-    let lastName = document.querySelector("last-name");
-    let email = document.querySelector("email");
-
-}
+let link = document.querySelector('a');
+link.addEventListener("click", function(event) {
+  event.preventDefault();
+});
 
 //creer un objet et afficher ses propriétés avec des fonctions
-
 
 let user = {
   name: 'Camille',
